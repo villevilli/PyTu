1d1377f54faab19c415186c02d2ba13f28dd02b1
villevilli <kujala.ville.vk@gmail.com>
2016-11-26 10:31:02 +0200

    hyva

--- turtle piirrin.py
+++ turtle piirrin.py
@@ -5,24 +5,19 @@ Created on Sat Nov 26 09:43:20 2016
 @author: Ville
 """
 
-import turtle
+from turtle inport
 while vastaus != "lopeta":
-    print("sinuulla on", voitot ,"pistettä")
     vastaus = input("Jalka, Ydinase vai Torakka? (Lopeta lopettaa): ")
     vastaus = vastaus.lower()
     if vastaus == "lopeta":
-        print("Pelasit", laskuri, "kierrosta, joista voitit", voitot, "ja pelasit tasan", tasapelit, "peliä.")
+        print("loppu")
         break
-    if vastaus == ""
-    elif
-    elif vastaus == "ydinase" and tietokone == "jalka":
-        print("Voitit!")
-        voitot = voitot + 1
-        laskuri = laskuri + 1
-    elif vastaus == "torakka" and tietokone == "ydinase":
-        print("Voitit!")
-        voitot = voitot + 1
-        laskuri = laskuri + 1
+    if vastaus == "e":
+        forward(50)
+    elif vastaus == "v":
+        left(90)
+    elif vastaus == "o":
+        right(90)
     else:
-        print("Hävisit!")
-        laskuri = laskuri + 1
+        print("tuntematon komento")
+        
